669873a95d7cdbb8476ae7c6bc6e30e15873bb98
saakshiraut28 <saakshiraut28@gmail.com>
2023-06-05 22:35:39 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import {
+  Animator,
+  Fade,
+  FadeIn,
+  FadeOut,
+  Move,
+  MoveIn,
+  MoveOut,
+  ScrollContainer,
+  ScrollPage,
+  Sticky,
+  StickyIn,
+  StickyOut,
+  Zoom,
+  ZoomIn,
+  batch,
+} from "react-scroll-motion";
+import "./App.css";
+
+const ZoomInVar = batch(StickyIn(), FadeIn(), ZoomIn());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScrollContainer>
+      <ScrollPage>
+        <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -300))}>
+          <h2>Welcome to the series of Scroll Animations 👾</h2>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage>
+        <Animator animation={ZoomInVar}>
+          <h2>Introducing the react-motion-scroll dependency 🌟</h2>
+          <h3> Wooo - hoooo </h3>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage>
+        <Animator animation={StickyIn()}>
+          <Animator animation={MoveIn(0, 1000)}>
+            <h2>This is awesome 🤩</h2>
+          </Animator>
+          <br />
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h2>Left Slide</h2>
+          </Animator>
+          <br />
+          <Animator animation={MoveIn(1000, 0)}>
+            <h2>Right Slide</h2>
+          </Animator>
+        </Animator>
+      </ScrollPage>
+    </ScrollContainer>
   );
 }
 
